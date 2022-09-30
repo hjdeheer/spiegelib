c52a13c39733f3d4ed2e4b716ab70270a8e530df
96Asch <96Asch@github.com>
2022-09-30 18:33:18 +0200

    Remove Sequential model in favor of plain layers

--- src/spiegelib/estimator/linear.py
+++ src/spiegelib/estimator/linear.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 """
-A Linear backbone for processing features. From Sound2Synth (Chen, Ying et al, 2022)
+A Linear backbone for processing features. \
+From Sound2Synth (Chen, Ying et al, 2022)
 """
 
 import tensorflow as tf
@@ -13,14 +14,20 @@ class LinearBackBone(layers.Layer):
     def __init__(self, input_dim, hidden_dim=2048, output_dim=2048):
         super(LinearBackBone, self).__init__()
         
-        self.model = tf.keras.Sequential()
-        self.model.add(layers.Dense(hidden_dim, use_bias=True))
-        self.model.add(layers.LeakyReLU(alpha=1e-2))
-        self.model.add(layers.Dense(output_dim, use_bias=True))
-        self.model.add(layers.LeakyReLU(alpha=1e-2))
+        self.dense1 = layers.Dense(hidden_dim, use_bias=True)
+        self.act1 = layers.LeakyReLU(alpha=1e-2)
+        self.dense2 = layers.Dense(output_dim, use_bias=True)
+        self.act2 = layers.LeakyReLU(alpha=1e-2)
 
     
     def call(self, inputs):
         y = inputs
         x = tf.reshape(y, [y.shape[0], -1])
-        return self.model(x)
+        
+        x = self.dense1(x)
+        x = self.act1(x)
+        x = self.dense2(x)
+        x = self.act1(x)
+
+        return x
+        
