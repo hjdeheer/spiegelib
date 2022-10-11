8698f0860a2f8d1f2ac07082e4459e17708064ef
hjdeheer <hugodeheer1234@gmail.com>
2022-10-11 13:34:21 +0200

    Update eval to contain metric scores

--- src/eval.py
+++ src/eval.py
@@ -2,22 +2,26 @@ import numpy as np
 import tensorflow as tf
 import os
 
+from matplotlib import pyplot as plt
+
 import spiegelib as spgl
 
 def main():
-    synth_path = "/home/spiegelib/vsts/dexed-0.9.6-lnx/Dexed.vst3"
-    model_path = "/home/spiegelib/data/models/conv8_STFT_uniform/cnn_vgg11_STFT_uniform.h5"
+    synth_path = "../vsts/Dexed.dll"
+    model_path = "../data/models/conv6_STFT_uniform9/model.h5"
 
-    source_path = "/home/spiegelib/data/evaluation/audio"
-    save_path  = "/home/spiegelib/data/eval_test"
+    source_path = "../data/evaluation/audio"
+    save_path  = "../data/evaluation/predict"
 
     synth = spgl.synth.SynthDawDreamer(synth_path,
                                         note_length_secs=1.0,
                                         render_length_secs=1.0)
 
+    synth.load_state("../vsts/op2_dexed.json")
     network = spgl.estimator.TFEstimatorBase.load(model_path)
 
     extractor = spgl.features.STFT(output='magnitude', fft_size=512, hop_size=256, time_major=True)
+    extractor.load_scaler("D:/data_uniform/STFT/data_scaler.pkl")
     extractor.add_modifier(lambda data : data.reshape(data.shape[0], data.shape[1], 1), type='output')
 
     matcher = spgl.SoundMatch(synth, network, extractor)   
@@ -27,6 +31,17 @@ def main():
     for i in range(len(targets)):
         audio = matcher.match(targets[i])
         audio.save(os.path.join(save_path, f'{i}.wav'))
+
+
+    #Perform evaluation
+    estimations  = [spgl.AudioBuffer.load_folder(save_path)]
+    evaluation = spgl.evaluation.MFCCEval(targets, estimations)
+    evaluation.evaluate()
+    print(evaluation.get_scores())
+    print(evaluation.get_stats())
+    bins = np.arange(0, 100, 2.5)
+    evaluation.plot_hist([0], 'mean_abs_error', bins)
+    plt.show()
 
 if __name__ == "__main__":
     main()
