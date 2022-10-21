2c5e06cab8946278d68075ba0d23ab1891ada144
96Asch <96Asch@github.com>
2022-10-21 02:10:40 +0200

    Add parameter weighting calculations

--- src/param_weight.py
+++ src/param_weight.py
@@ -1,8 +1,21 @@
+from distutils.command.config import config
 import numpy as np
+from tqdm import trange
 from spiegelib.synth import SynthDawDreamer
+from spiegelib.evaluation import MFCCEval
+import json
 import os
 
+
 def generate_default_overidden_params( ):
+    """
+        Generate the default overridden parameters for the Dexed synthesizer.
+        Initial config is taken from the Spiegelib guide.
+
+        :return: List[Tuple[Int, Float]] List of overridden parameters. These parameters will
+        not change when randomize_patch is called.
+    """
+
     algorithm_number = 1
     alg = (1.0 / 32.0) * float(algorithm_number - 1) + 0.001
 
@@ -72,34 +85,91 @@ def generate_default_overidden_params( ):
     # Override operators 3 through 6
     overridden_parameters.extend([(i, 0.0) for i in range(67, 155)])
     return overridden_parameters
+    
+def generate_parameter_weights(synth, n_samples=5000, values=np.arange(0, 1.1, 0.1).round(1)):
+    """
+        Generate the parameter weights by calculate the MFCC error over different configurations 
+        denoted by values. Each parameter in parameters_to_weight is fixed to values.
 
+        :synth: spiegelib.synth.SynthDawDreamer, the synthesizer used to generate sounds.
+        :n_samples: Int, the number of samples to generalize over.
+        :values: List[Float], the values with which to fix a parameter to.
 
-def generate_configurations(synth, values=np.arange(0, 1, 0.1)):
-    config_path = "../data/param_weighting/configs"
-
+        :return: Dict[Int, Float], dictionary containing the weighting for each parameter.
+    """
     default_overrides = generate_default_overidden_params()
     parameters_to_weight = [parameter for parameter in range(22, 155) 
-                                if parameter not in default_overrides]
-    parameter_description = synth.parameter_desc
-    print(parameters_to_weight)
+                                if parameter not in dict(default_overrides)]
+    weight_dict = {}
+
+    # Weight dictionary holds a list of size n_samples per parameter
     for parameter in parameters_to_weight:
-        for value in values:
-            overridden_parameters = default_overrides.copy()
-            overridden_parameters.append((parameter, value))
-            synth.set_overridden_parameters(overridden_parameters)
-            synth.save_state(os.path.join(config_path, f"{parameter_description[parameter]['name']}_{value}.json"))
+        weight_dict[parameter] = []
+
+    for _ in trange(0, n_samples):
+        # Set the default overridden parameters and randomize the patch    
+        synth.set_overridden_parameters(default_overrides)
+        synth.randomize_patch(technique="uniform")
+
+        patch = synth.patch
+
+        for parameter in parameters_to_weight:
+            parameter_audio = []
+
+            for value in values:
+                # Remove the current parameter from the patch list
+                new_patch = [(p, v) for (p, v) in patch if p != parameter]
+                # Add the current parameter with the specified value
+                new_patch.append((parameter, value))
+
+                # Load the new patch in and render the audio
+                synth.patch = new_patch
+                synth.load_patch()
+                synth.render_patch()
+                parameter_audio.append(synth.get_audio())
+
+          
+            mfcc_distance = calculate_mean_MFCC_error(parameter_audio)
+            # Add the mean MAE score to the dictionary
+            weight_dict[parameter].append(np.mean(mfcc_distance))
     
-def main():
-    pass
-    # synth.load_state(config_path)
-    # synth.randomize_patch()
+    weights = {}
+    # Average the per sample weight scores for each parameter
+    for (key, value) in weight_dict:
+        weights[key] =  np.mean(value)
+
+    return weights
+
+
+def calculate_mean_MFCC_error(audio_list, error_type="mean_abs_error"):
+    """
+    Calculate the mean MFCC error by pair-wise comparing audio samples
+
+    :audio_list: List[AudioBuffer], the list of generated samples
+    :error_type: Str, the error to be returned (See spiegelib.evaluation.EvalaluationBase)
+    """
+    # Do consecutive MFCC analysis on samples in parameter_audio
+    mfcc_distances = []
+    for audio_1, audio_2 in zip(audio_list, audio_list[1:]):
+        mfcc_eval = MFCCEval([audio_1], [[audio_2]])
+        mfcc_eval.evaluate()
+
+        # Extract the MAE score from the MFCC Eval class
+        mfcc_distances.append(mfcc_eval.get_scores()['target_0']['source_0'][error_type])  
+
+    return np.mean(mfcc_distances)  
 
 
 
 if __name__ == '__main__':
     vst_path = os.path.join("..", "vsts", "Dexed.dll")
-    print(vst_path)
     synth = SynthDawDreamer(vst_path, note_length_secs=1.0, render_length_secs=1.0)
 
-    generate_configurations(synth)
-    main()
+    config_path = "../data/param_weighting/configs"
+
+    weights = generate_parameter_weights(synth)
+
+    print(weights)
+
+    with open('weights.json', 'w', encoding='utf-8') as file:
+        json.dump(weights, file, ensure_ascii=False, indent=4)
